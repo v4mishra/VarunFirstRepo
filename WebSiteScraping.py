2b75edd08f6e4eb6f40f9b426076aa794532b2ce
v4mishra <v4mishra@gmail.com>
2016-10-07 17:59:43 +0530

    added some spaces.

--- WebSiteScraping.py
+++ WebSiteScraping.py
@@ -3,8 +3,11 @@ import requests
 
 page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 tree = html.fromstring(page.content)
+
+
 #This will create a list of buyers:
 buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+
 #This will create a list of prices
 prices = tree.xpath('//span[@class="item-price"]/text()')
 
